8ac56e1d6bfbb47541f3e5de208fa7d75b50595f
Priyanshu Ranjan <39668141+matumba78@users.noreply.github.com>
2026-06-12 20:10:47 +0530

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        distance = []
+        hash_map = {}
+        for i in range(len(points)):
+            res = -math.sqrt(points[i][0]*points[i][0] + points[i][1]*points[i][1])
+
+            distance.append(res)
+            if not hash_map.get((res), None):
+                hash_map[(res)] = [points[i]]
+            else:
+                hash_map[(res)].append(points[i])
+        heapq.heapify(distance)
+        while len(distance) > k:
+            heapq.heappop(distance)
+        print(distance)
+        print(hash_map)
+        r = []
+        for i in range(len(distance)):
+            if distance[i] in hash_map:
+                r += hash_map[distance[i]]
+                del hash_map[distance[i]]
+        return r
