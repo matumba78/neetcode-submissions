4f6d2baff4417499a9e4970d9180c88eb791fb39
Priyanshu Ranjan <39668141+matumba78@users.noreply.github.com>
2026-06-18 23:52:20 +0530

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        adj_list = [[] for _ in range(n)]
+
+        for u, v in edges:
+            adj_list[u].append(v)
+            adj_list[v].append(u)
+
+        q = deque()
+        q.append([0, -1]) 
+        # and node and parent so that we can skil if coming from parent as this is un directed graph
+        visit = set()
+        while q:
+            node, parent = q.popleft()
+            visit.add(node)
+            for nei in adj_list[node]:
+                if nei == parent:
+                    continue
+                if nei in visit:
+                    return False
+                visit.add(nei)
+                q.append([nei, node])
+        return len(visit) == n
+
+
+        
